bdaa5255a47782d756936bed67466e0b7891af3d
david <davidbuiza@gmail.com>
2020-05-15 12:06:25 +0200

    Commit1

---
+++ src/main/java/ArraysI.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+
+
+public class ArraysI {
+    
+    
+    ArrayList<String> listaPaises = new ArrayList<String>();
+    
+    
+    
+    public static void main(String[] args) {
+        
+       
+    }
+    
+    public String paisMasLargo(){
+        
+        return "";
+    }
+    
+     
+}
+
